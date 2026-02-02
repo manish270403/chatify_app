e215705693e92229d34c4702e5fc6641dff0766f
harsh <manish57558@gmail.com>
2026-02-03 00:53:14 +0530

    home route

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,6 +28,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -35,6 +36,13 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
+app.get("/", (req, res) => {
+  res.send({
+    activeStatus: true,
+    error: false,
+  });
+});
+
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
